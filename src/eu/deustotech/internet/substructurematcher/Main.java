e7d8b5efc8bc1689f1e2edf4bc8ab51b4189a97c
Mikel Emaldi <m.emaldi@gmail.com>
2013-03-14 15:40:29 +0100

    LabeledGraph structure created.

--- src/eu/deustotech/internet/substructurematcher/Main.java
+++ src/eu/deustotech/internet/substructurematcher/Main.java
@@ -9,7 +9,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 
@@ -18,6 +23,9 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.semanticweb.owl.align.AlignmentProcess;
+import org.semanticweb.owl.align.Cell;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -30,25 +38,50 @@ public class Main {
 
 	static String substructureDir = "";
 	static Set<String> alignmentClasses = new HashSet<String>();
+	static Logger logger = LoggerFactory.getLogger(Main.class);
+	static Set<String> entities = new HashSet<String>();
+	static Set<LabeledGraph> labeledGraphs = new HashSet<LabeledGraph>();
 	
 	public static void main(String args[]) {
+		
+		logger.info("Processing substructures...");
 		
 		//TODO: use args[]
 		loadConfigXML("config.xml");
 		
 		Set<URI> ontologySet = searchOntologies(substructureDir);
 		
+		Set<AlignmentProcess> alignmentSet = new HashSet<AlignmentProcess>();
+		List<Cell> matchedCells = new ArrayList<Cell>();
 		
 		for (URI onto1 : ontologySet) {
 			for (URI onto2 : ontologySet) {
 				if (onto1 != onto2) {
 					for (String alignmentClass : alignmentClasses) {
 						AlignmentProcess aProcess = applyAlignment(onto1, onto2, alignmentClass);
+						alignmentSet.add(aProcess);
+						Enumeration<Cell> cells = aProcess.getElements();
+						while(cells.hasMoreElements()) {
+							Cell cell = cells.nextElement();
+							if (entities.contains(cell.getObject1().toString()) || entities.contains(cell.getObject2().toString())) {								
+								matchedCells.add(cell);
+							}				
+						}
 					}
 				}
 			}
 		}
-		//System.out.println(ontologySet);
+		
+		logger.info("Matching subgraphs...");
+		
+		for (LabeledGraph graph1 : labeledGraphs) {
+			for (LabeledGraph graph2 : labeledGraphs) {
+				if (graph1 != graph2) {
+					
+				}
+			}
+		}
+		
 	
 	}
 
@@ -73,14 +106,11 @@ public class Main {
 	        }
 	        
 		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.error(e.getMessage());
 		} catch (SAXException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.error(e.getMessage());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.error(e.getMessage());
 		}
 	}
 
@@ -92,15 +122,13 @@ public class Main {
 			aProcess =  new  NameAndPropertyAlignment();
 			Properties params = new BasicParameters();
 			
-			System.out.println(onto1 + " - " + onto2);
+			logger.info(String.format("Creating alignment between <%s> and <%s>", onto1, onto2));
 			
 			aProcess.init(onto1, onto2);
 			aProcess.align(null, params);
 			
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.out.println("Error processing " + onto1 + " - " + onto2);
+			logger.error(e.getMessage());
 		}
 		return aProcess;
 	}
@@ -109,6 +137,13 @@ public class Main {
 		File folder = new File(substructureDir);
 		
 		Set<URI> ontologySet = new HashSet<URI>();
+		SecureRandom prng = null;
+		try {
+			prng = SecureRandom.getInstance("SHA1PRNG");
+		} catch (NoSuchAlgorithmException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
 		for (final File fileEntry : folder.listFiles()) {
 			try {
@@ -116,13 +151,19 @@ public class Main {
 				DataInputStream inputStream = new DataInputStream(fstream);
 				BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 				String strLine;
+				LabeledGraph graph = new LabeledGraph(prng.nextInt());
 				while((strLine = br.readLine()) != null) {
 					URI uri = null;
 					if (strLine.startsWith("v")) {
 						uri = new URI(strLine.substring(4));
+						Vertex vertex = new Vertex(Integer.parseInt(strLine.substring(2, 3)), uri.toString());
+						graph.addVertex(vertex);
 					} else if (strLine.startsWith("d")) {
 						uri = new URI(strLine.substring(6));
+						Edge edge = new Edge(Integer.parseInt(strLine.substring(2, 3)), Integer.parseInt(strLine.substring(4, 5)), uri.toString());
+						graph.addEdge(edge);
 					}
+					entities.add(String.format("<%s>", uri));
 					
 					if (uri.toURL().getRef() != null) { 
 						ontologySet.add(new URI(uri.toString().split("#")[0] + "#"));
@@ -135,15 +176,13 @@ public class Main {
 						ontologySet.add(new URI(ontologyURL));
 					}
 				}
+				labeledGraphs.add(graph);
 			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				logger.error(e.getMessage());
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				logger.error(e.getMessage());
 			} catch (URISyntaxException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				logger.error(e.getMessage());
 			}
 		}
 		
