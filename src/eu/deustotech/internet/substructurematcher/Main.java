60cd13cf33594e3340aca4b92fa6dc70a380fbf6
Mikel Emaldi <m.emaldi@gmail.com>
2013-03-15 10:57:46 +0100

    Labels from diferent ontologies matched.

--- src/eu/deustotech/internet/substructurematcher/Main.java
+++ src/eu/deustotech/internet/substructurematcher/Main.java
@@ -7,14 +7,18 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
@@ -40,49 +44,63 @@ public class Main {
 	static Set<String> alignmentClasses = new HashSet<String>();
 	static Logger logger = LoggerFactory.getLogger(Main.class);
 	static Set<String> entities = new HashSet<String>();
-	static Set<LabeledGraph> labeledGraphs = new HashSet<LabeledGraph>();
+	
 	
 	public static void main(String args[]) {
 		
 		logger.info("Processing substructures...");
 		
+		Map<Tuple<String, String, String>, AlignmentProcess> alignmentMap = new HashMap<Tuple<String, String, String>, AlignmentProcess>();
+		
 		//TODO: use args[]
 		loadConfigXML("config.xml");
-		
-		Set<URI> ontologySet = searchOntologies(substructureDir);
-		
-		Set<AlignmentProcess> alignmentSet = new HashSet<AlignmentProcess>();
-		List<Cell> matchedCells = new ArrayList<Cell>();
-		
-		for (URI onto1 : ontologySet) {
-			for (URI onto2 : ontologySet) {
-				if (onto1 != onto2) {
-					for (String alignmentClass : alignmentClasses) {
-						AlignmentProcess aProcess = applyAlignment(onto1, onto2, alignmentClass);
-						alignmentSet.add(aProcess);
-						Enumeration<Cell> cells = aProcess.getElements();
-						while(cells.hasMoreElements()) {
-							Cell cell = cells.nextElement();
-							if (entities.contains(cell.getObject1().toString()) || entities.contains(cell.getObject2().toString())) {								
-								matchedCells.add(cell);
-							}				
+		List<LabeledGraph> labeledGraphs = parseGraphs(substructureDir);
+		for (LabeledGraph graph1 : labeledGraphs) {
+			Set<URI> graph1Ontologies = searchOntologies(graph1);
+			for (LabeledGraph graph2 : labeledGraphs) {
+				if (!graph1.equals(graph2)) {
+					Set<URI> graph2Ontologies = searchOntologies(graph2);
+					for (URI ontology1 : graph1Ontologies) {
+						for (URI ontology2 : graph2Ontologies) {
+							for (String alignmentMethod : alignmentClasses) {
+								AlignmentProcess aProcess = null;
+								if (!alignmentMap.containsKey(new Tuple<String, String, String>(ontology1.toString(), ontology2.toString(), alignmentMethod))) {
+									aProcess = applyAlignment(ontology1, ontology2, alignmentMethod);
+									alignmentMap.put(new Tuple<String, String, String>(ontology1.toString(), ontology2.toString(), alignmentMethod), aProcess);
+								} else {
+									aProcess = alignmentMap.get(new Tuple<String, String, String>(ontology1.toString(), ontology2.toString(), alignmentMethod));					
+								}
+								Set<String> sourceLabels = getGraphLabels(graph1);
+								Set<String> targetLabels = getGraphLabels(graph2);
+								Enumeration<Cell> cells = aProcess.getElements();
+								
+								while (cells.hasMoreElements()) {
+									Cell cell = cells.nextElement();
+									String label1 = cell.getObject1().toString().replace("<", "").replace(">", "");
+									String label2 = cell.getObject2().toString().replace("<", "").replace(">", "");
+									if (sourceLabels.contains(label1) && targetLabels.contains(label2)) {
+										System.out.println(String.format("%s - %s (%s)", label1, label2, cell.getStrength()));
+									}
+								}
+							}
 						}
 					}
 				}
 			}
 		}
+		System.out.println(alignmentMap.size());
 		
-		logger.info("Matching subgraphs...");
-		
-		for (LabeledGraph graph1 : labeledGraphs) {
-			for (LabeledGraph graph2 : labeledGraphs) {
-				if (graph1 != graph2) {
-					
-				}
-			}
+	}
+
+	private static Set<String> getGraphLabels(LabeledGraph graph) {
+		Set<String> sourceLabels = new HashSet<String>();
+		for (Vertex vertex : graph.geVertex()) {
+			sourceLabels.add(vertex.getLabel());
 		}
-		
-	
+		for (Edge edge : graph.getEdges()) {
+			sourceLabels.add(edge.getLabel());
+		}
+		return sourceLabels;
 	}
 
 	private static void loadConfigXML(String configFile) {
@@ -132,11 +150,10 @@ public class Main {
 		}
 		return aProcess;
 	}
-
-	private static Set<URI> searchOntologies(String substructureDir) {
+	
+	private static List<LabeledGraph> parseGraphs(String substructureDir) {
 		File folder = new File(substructureDir);
-		
-		Set<URI> ontologySet = new HashSet<URI>();
+		List<LabeledGraph> graphs = new ArrayList<LabeledGraph>();
 		SecureRandom prng = null;
 		try {
 			prng = SecureRandom.getInstance("SHA1PRNG");
@@ -163,29 +180,60 @@ public class Main {
 						Edge edge = new Edge(Integer.parseInt(strLine.substring(2, 3)), Integer.parseInt(strLine.substring(4, 5)), uri.toString());
 						graph.addEdge(edge);
 					}
-					entities.add(String.format("<%s>", uri));
-					
-					if (uri.toURL().getRef() != null) { 
-						ontologySet.add(new URI(uri.toString().split("#")[0] + "#"));
-					} else {
-						String [] splittedURL = uri.toString().split("/");
-						String ontologyURL = "";
-						for (int i = 0; i < splittedURL.length - 1; i++) {
-							ontologyURL += splittedURL[i] + "/";
-						}
-						ontologySet.add(new URI(ontologyURL));
-					}
 				}
-				labeledGraphs.add(graph);
+				graphs.add(graph);
 			} catch (FileNotFoundException e) {
 				logger.error(e.getMessage());
 			} catch (IOException e) {
 				logger.error(e.getMessage());
 			} catch (URISyntaxException e) {
-				logger.error(e.getMessage());
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+		}
+		
+		
+		return graphs;
+	}
+	
+	private static URI getPrefix(URI uri) {
+		try {
+			if (uri.toURL().getRef() != null) { 
+				return new URI(uri.toString().split("#")[0] + "#");
+			} else {
+				String [] splittedURL = uri.toString().split("/");
+				String ontologyURL = "";
+				for (int i = 0; i < splittedURL.length - 1; i++) {
+					ontologyURL += splittedURL[i] + "/";
+				}
+				return new URI(ontologyURL);
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return uri;
+	}
+	
+	private static Set<URI> searchOntologies(LabeledGraph graph) {
+		
+		Set<URI> ontologySet = new HashSet<URI>();
+		try {
+			for (Vertex vertex : graph.geVertex()) {
+				ontologySet.add(getPrefix(new URI(vertex.getLabel())));
+			}
+			for (Edge edge : graph.getEdges()) {
+				ontologySet.add(getPrefix(new URI(edge.getLabel())));
+			}
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
 		}
 		
 		return ontologySet;
 	}
+
+	
 }
